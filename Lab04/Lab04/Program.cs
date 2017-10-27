566a8efa9982125a8b228948cf3ff574db9f7ebe
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-27 16:40:39 +0700

    Multiplication table

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -2,13 +2,13 @@
 
 namespace ConsoleApp2
 {
-    class ForLoopTest
+    class Multiply
     {
         static void Main()
         {
-            for (int i = 1; i <= 5; i++)
+            for (int i = 1; i <= 12; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("{0, 2} x 2 = {1, 2}", i, i * 2);
             }
         }
     }
