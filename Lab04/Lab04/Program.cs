0114ca8c8c273d8fc80445ccba810237bdd30475
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-27 16:35:40 +0700

    while

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,40 +1,17 @@
 ﻿using System;
-using System.IO;
-public class switchLearning
-{
-    public static void Main()
-    {
-        Console.Write("Input Day Name (sun, mon, tue, wed, thu, fri and sat) : ");
-        string day = Console.ReadLine();
-        string message;
-        switch (day)
 
+namespace ConsoleApp2
+{
+    class WhileTest
+    {
+        static void Main()
         {
-            case "sun":
-                message = "Sun is Sunday, color Red";
-                break;
-            case "mon":
-                message = "Mon is Monday, color Yellow";
-                break;
-            case "tue":
-                message = "Tue is Tuesday, color Pink";
-                break;
-            case "wed":
-                message = "Wed is Wednesday, color Green";
-                break;
-            case "thu":
-                message = "Thu is Thursday, color Orange";
-                break;
-            case "fri":
-                message = "Fri is Friday, color Blue";
-                break;
-            case "sat":
-                message = "Sat is Saturday, color Purple";
-                break;
-            default:
-                message = "Incorrect Day Name";
-                break;
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine("Current value of n is {0}", n);
+                n++;
+            }
         }
-        Console.WriteLine(message);
     }
 }
