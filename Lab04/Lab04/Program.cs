4b43b771d34b0ff81bb79c11a0005a06e1e48df0
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-27 16:39:02 +0700

    for

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -2,28 +2,14 @@
 
 namespace ConsoleApp2
 {
-    class DoTest
+    class ForLoopTest
     {
         static void Main()
         {
-            Console.WriteLine("---- Begin of While statement.");
-            int n = 1;
-            while (n < 1)
+            for (int i = 1; i <= 5; i++)
             {
-                Console.WriteLine("  Current value of n is {0}", n);
-                n++;
+                Console.WriteLine(i);
             }
-            Console.WriteLine("---- End of While statement.");
-
-            Console.WriteLine("---- Begin of Do..While statement.");
-            n = 1;
-            do
-            {
-                Console.WriteLine("  Current value of n is {0}", n);
-                n++;
-            }
-            while (n < 1);
-            Console.WriteLine("---- End of Do..While statement.");
         }
     }
 }
