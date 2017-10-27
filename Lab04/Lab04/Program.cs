91de17cf7cd4005d390a545b4d5d76573dd253da
ศรัญญู ตำลิ่ม <deathnote_boat@hotmail.com>
2017-10-27 15:55:38 +0700

    Lab04P03-1

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -6,10 +6,31 @@ public class ExceptionLearning
     {
         int a = 10;
         int b = 20;
-        int c = add(a, b);
+        int c;
+        try
+        {
+            c = div(a, b);
+        }
+        catch (DivideByZeroException e)
+        {
+
+            Console.WriteLine("DivideByZeroException");
+            Console.WriteLine(e.Message);
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine("NullReferenceException");
+            Console.WriteLine(e.Message);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception");
+            Console.WriteLine(e.Message);
+        }
     }
-    private static int add(int a, int b)
+    private static int div(int a, int b)
     {
-        throw new NotImplementedException();
+        throw new DivideByZeroException();
     }
 }
